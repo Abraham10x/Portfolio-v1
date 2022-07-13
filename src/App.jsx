f9dc3e49800eebb0ca022070b741ac8958536de4
Abraham10x <amehabraham811@gmail.com>
2022-07-13 14:03:22 +0100

    adding the About section components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
+import About from './components/About'
 import './App.css'
 
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Header/>
+      <About/>
     </div>
   )
 }
